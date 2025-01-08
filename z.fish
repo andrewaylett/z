2467fa749afe7aed238d3111d970ded2c8e707db
Andrew Aylett <andrew@aylett.co.uk>
2025-01-08 20:27:26 +0000

    Claude's second attempt, with awk

--- z.fish
+++ z.fish
@@ -23,6 +23,16 @@ function _z -d "Jump to a recent directory."
         return
     end
 
+    function _z_dirs
+        [ -f "$datafile" ]; or return
+        while read -l line
+            set -l dir (string split '|' $line)[1]
+            if test -d "$dir"
+                echo $line
+            end
+        end < "$datafile"
+    end
+
     # Add entry
     if test "$argv[1]" = "--add"
         set -e argv[1]
@@ -45,48 +55,51 @@ function _z -d "Jump to a recent directory."
         # Add entry to datafile
         set -l tempfile "$datafile.$RANDOM"
         
-        if test -f "$datafile"
-            awk -v path="$dir" -v now=(date +%s) -v score=$_Z_MAX_SCORE -F"|" '
-                BEGIN {
-                    rank[path] = 1
-                    time[path] = now
+        _z_dirs | awk -v path="$dir" -v now=(date +%s) -v score=$_Z_MAX_SCORE -F"|" '
+            BEGIN {
+                rank[path] = 1
+                time[path] = now
+            }
+            $2 >= 1 {
+                if( $1 == path ) {
+                    rank[$1] = $2 + 1
+                    time[$1] = now
+                } else {
+                    rank[$1] = $2
+                    time[$1] = $3
                 }
-                $2 >= 1 {
-                    if( $1 == path ) {
-                        rank[$1] = $2 + 1
-                        time[$1] = now
-                    } else {
-                        rank[$1] = $2
-                        time[$1] = $3
-                    }
-                    count += $2
-                }
-                END {
-                    if( count > score ) {
-                        for( x in rank ) print x "|" 0.99*rank[x] "|" time[x]
-                    } else for( x in rank ) print x "|" rank[x] "|" time[x]
-                }
-            ' "$datafile" 2>/dev/null >$tempfile
-            
-            if test $status -eq 0
-                mv -f "$tempfile" "$datafile"
-            else
-                rm -f "$tempfile"
+                count += $2
+            }
+            END {
+                if( count > score ) {
+                    for( x in rank ) print x "|" 0.99*rank[x] "|" time[x]
+                } else for( x in rank ) print x "|" rank[x] "|" time[x]
+            }
+        ' 2>/dev/null >$tempfile
+
+        if test $status -eq 0
+            if test -n "$_Z_OWNER"
+                chown $_Z_OWNER:"$(id -ng $_Z_OWNER)" "$tempfile"
             end
+            mv -f "$tempfile" "$datafile"
         else
-            echo "$dir|1|"(date +%s) >$datafile
+            rm -f "$tempfile"
         end
 
     # Complete
     else if test "$argv[1]" = "--complete"
-        if test -f "$datafile"
-            while read -l line
-                set -l dir (string split '|' $line)[1]
-                if test -d "$dir"
-                    echo $dir
-                end
-            end < "$datafile"
-        end
+        _z_dirs | awk -v q="$argv[2]" -F"|" '
+            BEGIN {
+                q = substr(q, 3)
+                if( q == tolower(q) ) imatch = 1
+                gsub(/ /, ".*", q)
+            }
+            {
+                if( imatch ) {
+                    if( tolower($1) ~ q ) print $1
+                } else if( $1 ~ q ) print $1
+            }
+        ' 2>/dev/null
 
     # List/Search
     else
@@ -94,52 +107,116 @@ function _z -d "Jump to a recent directory."
         set -l list
         set -l echo
         set -l fnd
+        set -l last
 
-        # Parse options
-        set -l options "h/help" "l/list" "r/rank" "t/recent" "e/echo" "c/current" "x/delete"
-        argparse $options -- $argv
-
-        if set -q _flag_help
-            echo "Usage: $_Z_CMD [-cehlrtx] args..." >&2
-            return
+        # Parse arguments
+        while test (count $argv) -gt 0
+            switch $argv[1]
+                case '--'
+                    set -e argv[1]
+                    set fnd (string join ' ' $argv)
+                    break
+                case '-*'
+                    set -l opt (string sub -s 2 $argv[1])
+                    while test -n "$opt"
+                        switch (string sub -s 1 -l 1 $opt)
+                            case 'c'
+                                set fnd "^$PWD $fnd"
+                            case 'e'
+                                set echo 1
+                            case 'h'
+                                echo "$_Z_CMD [-cehlrtx] args" >&2
+                                return
+                            case 'l'
+                                set list 1
+                            case 'r'
+                                set typ "rank"
+                            case 't'
+                                set typ "recent"
+                            case 'x'
+                                sed -i -e "\:^$PWD|.*:d" "$datafile"
+                                return
+                        end
+                        set opt (string sub -s 2 $opt)
+                    end
+                case '*'
+                    set fnd "$fnd $argv[1]"
+            end
+            set last $argv[1]
+            set -e argv[1]
         end
-
-        set -q _flag_list; and set list 1
-        set -q _flag_echo; and set echo 1
-        set -q _flag_rank; and set typ "rank"
-        set -q _flag_recent; and set typ "recent"
-        
-        if set -q _flag_current
-            set fnd "^$PWD $fnd"
-        end
-
-        if set -q _flag_delete
-            sed -i -e "\:^$PWD|.*:d" "$datafile"
-            return
-        end
-
-        # Build search string from remaining arguments
-        for arg in $argv
-            set fnd "$fnd $arg"
-        end
-        set fnd (string trim "$fnd")
 
         [ -f "$datafile" ]; or return
+        
+        set -l cd (_z_dirs | awk -v t=(date +%s) -v list="$list" -v typ="$typ" -v q="$fnd" -F"|" '
+            function frecent(rank, time) {
+                dx = t - time
+                return int(10000 * rank * (3.75/((0.0001 * dx + 1) + 0.25)))
+            }
+            function output(matches, best_match, common) {
+                if( list ) {
+                    if( common ) {
+                        printf "%-10s %s\n", "common:", common > "/dev/stderr"
+                    }
+                    cmd = "sort -n >&2"
+                    for( x in matches ) {
+                        if( matches[x] ) {
+                            printf "%-10s %s\n", matches[x], x | cmd
+                        }
+                    }
+                } else {
+                    if( common && !typ ) best_match = common
+                    print best_match
+                }
+            }
+            function common(matches) {
+                # find the common root of a list of matches, if it exists
+                for( x in matches ) {
+                    if( matches[x] && (!short || length(x) < length(short)) ) {
+                        short = x
+                    }
+                }
+                if( short == "/" ) return
+                for( x in matches ) if( matches[x] && index(x, short) != 1 ) {
+                    return
+                }
+                return short
+            }
+            BEGIN {
+                gsub(" ", ".*", q)
+                hi_rank = ihi_rank = -9999999999
+            }
+            {
+                if( typ == "rank" ) {
+                    rank = $2
+                } else if( typ == "recent" ) {
+                    rank = $3 - t
+                } else rank = frecent($2, $3)
+                if( $1 ~ q ) {
+                    matches[$1] = rank
+                } else if( tolower($1) ~ tolower(q) ) imatches[$1] = rank
+                if( matches[$1] && matches[$1] > hi_rank ) {
+                    best_match = $1
+                    hi_rank = matches[$1]
+                } else if( imatches[$1] && imatches[$1] > ihi_rank ) {
+                    ibest_match = $1
+                    ihi_rank = imatches[$1]
+                }
+            }
+            END {
+                # prefer case sensitive
+                if( best_match ) {
+                    output(matches, best_match, common(matches))
+                    exit
+                } else if( ibest_match ) {
+                    output(imatches, ibest_match, common(imatches))
+                    exit
+                }
+                exit(1)
+            }
+        ')
 
-        set -l cd (while read -l line
-            set -l parts (string split '|' $line)
-            set -l dir $parts[1]
-            set -l rank $parts[2]
-            set -l time $parts[3]
-
-            if test -d $dir
-                if string match -q -- "*$fnd*" $dir
-                    echo "$rank|$time|$dir"
-                end
-            end
-        end < "$datafile" | sort -n -r | head -n 1 | cut -d'|' -f3)
-
-        if test -n "$cd"
+        if test $status -eq 0; and test -n "$cd"
             if test -n "$echo"
                 echo "$cd"
             else
